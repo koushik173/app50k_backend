8caa8c9182e8ac51fa6012d1e9962a16742fc80e
koushik173 <cse_2012020173@lus.ac.bd>
2023-10-14 20:24:04 +0600

    hr hired done

--- Controllers/hrControllers.js
+++ Controllers/hrControllers.js
@@ -5,34 +5,35 @@ const prisma = new PrismaClient();
 exports.takeInterview = async (req, res, next) => {
     const { expartEmail, zoomlink, expartid } = req.body;
     try {
-      await sendEmail(expartEmail, "Take Interview", zoomlink);
-      const updatedgetInterView = await prisma.expert.update({
-        where: { id: expartid },
-        data: {
-          getInterView: true,
-        },
-      });
-      res.json(updatedgetInterView);
+        await sendEmail(expartEmail, "Take Interview", zoomlink);
+        const updatedgetInterView = await prisma.expert.update({
+            where: { id: expartid },
+            data: {
+                getInterView: true,
+            },
+        });
+        res.json(updatedgetInterView);
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-  };
-  
+};
 
-exports.hireExpart=async(req,res,next)=>{
+
+exports.hireExpart = async (req, res, next) => {
     const { expartEmail, exEmail, exPass, expartid } = req.body;
     try {
-      await sendEmail(expartEmail, "Get hired", {"email":exEmail,"password":exPass});
-      const updatehired = await prisma.expert.update({
-        where: { id: expartid },
-        data: {
-          hired: true,
-        },
-      });
-      res.json(updatehired);
+        await sendEmail(expartEmail, "Get hired", JSON.stringify({ "email": exEmail, "password": exPass }));
+
+        const updatehired = await prisma.expert.update({
+            where: { id: expartid },
+            data: {
+                hired: true,
+            },
+        });
+        res.json(updatehired);
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 }
